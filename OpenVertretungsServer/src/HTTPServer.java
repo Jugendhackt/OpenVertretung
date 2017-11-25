2c7515b805108d2741842c3b6f2cc30c450d24a8
sevenmaster <sevenmaster@users.noreply.github.com>
2017-11-25 20:32:33 +0100

    Update HTTPServer.java

--- OpenVertretungsServer/src/HTTPServer.java
+++ OpenVertretungsServer/src/HTTPServer.java
@@ -7,14 +7,15 @@ import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
 import org.json.JSONArray;
+import org.json.JSONObject;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 public class HTTPServer
 {
 
     private static Db database;
 
-    public static void start() throws Exception
-    {
+    public static void start() throws Exception {
         database = new Db();
         HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
         server.createContext("/test", new MyHandler());
@@ -27,23 +28,37 @@ public class HTTPServer
         @Override
         public void handle(HttpExchange t) throws IOException
         {
+            JSONArray body = new JSONArray(t.getRequestBody());
             switch (t.getRequestMethod()){
                 case "POST":
-                    JSONArray body = new JSONArray(t.getRequestBody());
-                        if(body.get(0).toString() == "writeLines"){
-                            database.WriteZeilen(body);
+                    body.remove(0);
+                    if(body.get(0).toString() == "writeLines") {
+                            try {
+                                database.WriteZeile(body);
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                            }
                         }
                     break;
                 case "GET":
+                        String r = database.Read((JSONObject) body.get(0)).toString();
+                        sendResponse(200, r, t);
+
                     break;
                 default:
+                    t.sendResponseHeaders(403, 0);
                     break;
 
             }
-            String response = "This is the response";
-            t.sendResponseHeaders(200, response.length());
+            
+            sendResponse(200, "Standard response", t);
+
+        }
+
+        private void sendResponse(int code, String content, HttpExchange t)throws IOException{
+            t.sendResponseHeaders(code, content.length());
             OutputStream os = t.getResponseBody();
-            os.write(response.getBytes());
+            os.write(content.getBytes());
             os.close();
         }
     }

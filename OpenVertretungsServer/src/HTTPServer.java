c27c97f3731fdc15549ab9d2f870a22ae66a56be
MetaColon <timo@borners.de>
2017-11-25 23:50:09 +0100

    get / set

--- OpenVertretungsServer/src/HTTPServer.java
+++ OpenVertretungsServer/src/HTTPServer.java
@@ -1,23 +1,41 @@
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.io.StringWriter;
+import java.net.InetAddress;
 import java.net.InetSocketAddress;
+import java.util.Objects;
 
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
+
 import org.json.JSONArray;
 import org.json.JSONObject;
+import org.omg.IOP.Encoding;
+
+import jdk.nashorn.internal.parser.JSONParser;
+import sun.misc.IOUtils;
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+import sun.rmi.runtime.Log;
+
+import static java.lang.System.in;
 
 public class HTTPServer
 {
 
     private static Db database;
+    private static HttpServer server;
 
-    public static void start() throws Exception {
-        database = new Db();
-        HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
+    public static void start() throws Exception
+    {
+        database = new Db();;
+        server = HttpServer.create(new InetSocketAddress(InetAddress.getByName
+                ("10.23.41.176"),
+                8001), 0);
+
         server.createContext("/test", new MyHandler());
         server.setExecutor(null); // creates a default executor
         server.start();
@@ -28,34 +46,82 @@ public class HTTPServer
         @Override
         public void handle(HttpExchange t) throws IOException
         {
-            JSONArray body = new JSONArray(t.getRequestBody());
-            switch (t.getRequestMethod()){
+            //sendResponse(200, "erfolgreich", t);
+            System.out.println("Got request");
+            System.out.println("Body: " + t.getRequestBody() + "; Head: " + t.getRequestHeaders
+                ().values());
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(t.getRequestBody(),
+                    "UTF-8"));
+
+            final StringBuilder builder = new StringBuilder();
+            String line = null;
+            while ((line = reader.readLine()) != null)
+            {
+                builder.append(line);
+                builder.append("\n");
+            }
+
+            JSONArray body = new JSONArray(builder.toString());
+
+
+            System.out.println("Initial body: " + body);
+
+
+            switch (t.getRequestMethod())
+            {
                 case "POST":
+                    System.out.println("Request POST body: " + body.getString(0));
+                    String commandType = body.getString(0);
                     body.remove(0);
-                    if(body.get(0).toString() == "writeLines") {
-                            try {
-                                database.WriteZeile(body);
-                            } catch (Exception e) {
-                                e.printStackTrace();
-                            }
+                    if (Objects.equals(commandType, "writeLines"))
+                    {
+                        System.out.println("Request POST body 2: " + body.get(0));
+                        try
+                        {
+                            System.out.println("Response: " + database.WriteZeilen(body));
+                        } catch (Exception e)
+                        {
+                            e.printStackTrace();
                         }
+                    }
+                    else if (Objects.equals(commandType, "writeLehrer"))
+                    {
+                        System.out.println(database.WriteLehrer(body.getJSONObject(0)) ?
+                                "Successful" : "Not successful");
+                    }
+                    else if (Objects.equals(commandType, "writeArt"))
+                    {
+                        System.out.println(database.WriteLehrer(body.getJSONObject(0)) ?
+                                "Successful" : "Not successful");
+                    }
+                    else if (Objects.equals(commandType, "writeKurs"))
+                    {
+                        System.out.println(database.WriteKurs(body.getJSONObject(0)) ?
+                                "Successful" : "Not successful");
+                    }
                     break;
                 case "GET":
-                        String r = database.Read((JSONObject) body.get(0)).toString();
-                        sendResponse(200, r, t);
+                    System.out.println("Request GET body: " + body.get(0));
+                    String r = database.Read(body.getJSONObject(0)).toString();
+                    System.out.println("Request response: " + r);
+                    sendResponse(200, r, t);
 
                     break;
                 default:
+                    System.out.println("Default");
                     t.sendResponseHeaders(403, 0);
                     break;
 
             }
-            
+
             sendResponse(200, "Standard response", t);
 
         }
 
-        private void sendResponse(int code, String content, HttpExchange t)throws IOException{
+        private void sendResponse(int code, String content, HttpExchange t) throws IOException
+        {
+            //content = content.substring(0, 10);
             t.sendResponseHeaders(code, content.length());
             OutputStream os = t.getResponseBody();
             os.write(content.getBytes());

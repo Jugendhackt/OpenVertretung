c4a8e1f8e2a402d60576dab1689168608cef1f84
MetaColon <timo@borners.de>
2017-11-25 14:48:01 +0100

    Added room to git and fixed java OpenVertretungsServer

---
+++ OpenVertretungsServer/src/Db.java
@@ -0,0 +1,13 @@
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
+public class Db
+{
+
+}
--- OpenVertretungsServer/src/HTTPServer.java
+++ OpenVertretungsServer/src/HTTPServer.java
@@ -7,18 +7,22 @@ import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
 
-public class HTTPServer {
+public class HTTPServer
+{
 
-    public static void start() throws Exception {
+    public static void start() throws Exception
+    {
         HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
         server.createContext("/test", new MyHandler());
         server.setExecutor(null); // creates a default executor
         server.start();
     }
 
-    static class MyHandler implements HttpHandler {
+    static class MyHandler implements HttpHandler
+    {
         @Override
-        public void handle(HttpExchange t) throws IOException {
+        public void handle(HttpExchange t) throws IOException
+        {
             String response = "This is the response";
             t.sendResponseHeaders(200, response.length());
             OutputStream os = t.getResponseBody();

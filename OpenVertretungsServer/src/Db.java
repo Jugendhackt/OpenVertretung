47042982cea84ce9666c717da47bc1d6eaa9eaf0
MetaColon <timo@borners.de>
2017-11-25 14:51:52 +0100

    db class

--- OpenVertretungsServer/src/Db.java
+++ OpenVertretungsServer/src/Db.java
@@ -1,11 +1,4 @@
-
-import java.io.IOException;
-import java.io.OutputStream;
-import java.net.InetSocketAddress;
-
-import com.sun.net.httpserver.HttpExchange;
-import com.sun.net.httpserver.HttpHandler;
-import com.sun.net.httpserver.HttpServer;
+import java.sql.*;
 
 public class Db
 {
